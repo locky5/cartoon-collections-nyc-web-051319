a9ee711da4cfb08ad0076beba3c0e493ae975c66
Kevin Dai <daikevin19@yahoo.com>
2019-05-02 02:04:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,8 @@ end
 
 def summon_captain_planet(elements)
   elements.collect do |element|
-    element.capitalize
+    puts "#{element.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
